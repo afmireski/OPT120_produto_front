4e886056e18a76019aca06be3762b6750d47d292
afmireski <afmireski@outlook.com>
2024-11-04 20:34:11 -0300

    refactor: Update Product model

--- lib/src/models/products.dart
+++ lib/src/models/products.dart
@@ -38,9 +38,40 @@ final class Product extends Equatable {
       stock: json['stock'] as int,
       createdAt: DateTime.parse(json['created_at']),
       updatedAt: DateTime.parse(json['updated_at']),
-      deletedAt: json['deletedAt'] != null
-          ? DateTime.parse(json['deleted_at'])
-          : null,
+      deletedAt:
+          json['deletedAt'] != null ? DateTime.parse(json['deleted_at']) : null,
+    );
+  }
+
+  static Map<String, dynamic> toJson(Product product) {
+    return {
+      'id': product.id,
+      'description': product.description,
+      'price': product.price,
+      'stock': product.stock,
+      'created_at': product.createdAt.toIso8601String(),
+      'updated_at': product.updatedAt.toIso8601String(),
+      'deleted_at': product.deletedAt?.toIso8601String(),
+    };
+  }
+
+  Product copyWith({
+    int? id,
+    String? description,
+    int? price,
+    int? stock,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    DateTime? deletedAt,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      description: description ?? this.description,
+      price: price ?? this.price,
+      stock: stock ?? this.stock,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      deletedAt: deletedAt ?? this.deletedAt,
     );
   }
 }
